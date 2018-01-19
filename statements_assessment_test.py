04a9d5f5d59f28b853e0841e961f8a526149cdf7
pinagata <agata.pindych@gmail.com>
2018-01-19 12:06:59 +0100

    methods

--- statements_assessment_test.py
+++ statements_assessment_test.py
@@ -1,6 +1,9 @@
 # 1
 st = 'Print only the words that start with s in this sentence'
-# don't know
+for word in st.split():
+    if word[0] == 's':
+        print(word)
+
 
 # 2
 print(range(0,11,2))
@@ -19,6 +22,22 @@ print(lst)
 
 # 4
 st = 'Print every word in this sentence that has an even number of letters'
-for letter in st:
-    if letter % 2 == 0:
+for word in st.split():
+    if len(word) % 2 == 0:
+        print(word + ' <-- has an even number of letters')
 
+
+# 5
+for number in range(1,101):
+    if number % 5 == 0 and number % 3 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
+
+# 6
+st = 'Create a list of the first letters of every word in this string'
+print([word[0] for word in st.split()])
